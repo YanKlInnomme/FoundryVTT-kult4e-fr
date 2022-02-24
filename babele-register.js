9afd1b94f8dddf90cbe8bee16eb48b0b37c33f41
YanK l'Innommé <100078854+YanKlInnomme@users.noreply.github.com>
2022-02-24 09:04:27 +0100

    Update babele-register.js

--- babele-register.js
+++ babele-register.js
@@ -1,11 +1,3 @@
-Hooks.once("ready", () => {
-    // Listen for dice icon click
-    const diceIconSelector = '#chat-controls i.fas.fa-dice-d20';
-    $(document).on('click', diceIconSelector, () => { 
-        console.log(`Dice Icon Works`);
-    });
-});
-
 Hooks.on('init', () => {
     if(typeof Babele !== 'undefined') {
         Babele.get().register({
